822589086100acff64182628d9043efbd33bcc67
WissamZa <36477299+WissamZa@users.noreply.github.com>
2021-10-27 15:51:31 +0300

    add_installing_requirements

--- Google_form_auto-submit.py
+++ Google_form_auto-submit.py
@@ -1,16 +1,19 @@
+import subprocess
+
+if(subprocess.run("pip3 show requests", stdout=subprocess.DEVNULL,
+                  stderr=subprocess.DEVNULL).returncode == 1):
+    print("Install the library need it for the script...")
+    subprocess.run("pip3 install requests", stdout=subprocess.DEVNULL)
+    
+
 import requests
 import re
 from datetime import datetime, time
 import os
 import json
-
-
 def check_form(req):
     return re.search("closedform", req.text) == None
-
-
 def post():
-
     with open("info.json", 'r') as jason_file:
         data = json.load(jason_file)
         submission = data["data"]
@@ -20,7 +23,6 @@ def post():
         email = values[1]
         status = values[2]
         print(f"{url=}\n{name=}\n{email=}\n{status=}")
-
         req = requests.get(url)
         if (check_form(req)):
             sent = requests.post(url, submission)
@@ -35,24 +37,19 @@ def post():
                 print('An error has occurred  Code ' + str(sent.status_code))
         else:
             print("The form is close now try again later")
-
-
 def creatNewFile():
     # for SDA-E Cybersecurity Attendance form keep the url empty
     geturl = input(
         "Enter google form url you want to submit (keep it empty for the default value):\n")
     if geturl == "":
         url = "https://docs.google.com/forms/d/e/1FAIpQLSdDYw8ubu1Mhlab3FEEFyFxoTqNS7l9mwVYSgOfdBRsVCNsKg/formResponse"
-        
     else:
         url = re.search("(.*docs.google.com/forms/d/e/.*)/.*", geturl)
         url = url.group(1)+"/formResponse"
-
     req = requests.get(url)
     if (check_form(req)):
         e = re.findall(
             "(?:\"|)(?:Name|Email|Status)(?: \",|\",)null,\d,\[\[([0-9]+)", req.text)
-
         entry_Name = e[0]
         entry_Email = e[1]
         entry_Status = e[2]
@@ -68,15 +65,12 @@ def creatNewFile():
         }
         with open("info.json", "w") as jason_file:
             json.dump(json_data, jason_file)
-
         post()
     else:
         print("The form is close now try again later")
-
 if(os.path.exists('info.json')):
     post()
 else:
     print("info.json file not found creating new one...")
     creatNewFile()
-
 os.system("pause")
